3f9183f8900002a5b4d93f72eef75b4cecfa99aa
Lee <changer2000@sohu.com>
2019-05-12 16:45:27 +0800

    gradel study link

--- src/java/TestB.java
+++ src/java/TestB.java
@@ -1,5 +1,7 @@
 package java;
 
 public class TestB {
-
+	//https://yq.aliyun.com/articles/62362
+	//https://blog.csdn.net/aixiaoyang168/article/details/78027844
+	//https://www.cnblogs.com/davenkin/p/gradle-learning-1.html
 }
