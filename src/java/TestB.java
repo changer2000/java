1fd1e66a6b713ec4c499440c902f11329a485cc7
changer2000 <changer2000@sohu.com>
2019-05-13 09:42:36 +0800

    copy git work files and import the project, then commit it for testing

--- src/java/TestB.java
+++ src/java/TestB.java
@@ -4,4 +4,5 @@ public class TestB {
 	//https://yq.aliyun.com/articles/62362
 	//https://blog.csdn.net/aixiaoyang168/article/details/78027844
 	//https://www.cnblogs.com/davenkin/p/gradle-learning-1.html
+	//
 }
